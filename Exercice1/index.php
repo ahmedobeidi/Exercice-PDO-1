e3d4444f03840d42b814d47659bff1cf8997ed6b
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-09 13:34:33 +0100

    MODIFY: Exercic 1

--- Exercice1/index.php
+++ Exercice1/index.php
@@ -9,10 +9,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecercice 1</title>
+    <style>
+        div {
+            color: white; 
+            width: 150px;
+            display: flex;
+            flex-direction: column;
+            background-color: brown;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <?php 
-        var_dump($users);
+     
+    <?php foreach($users as $user) { ?>
+        <div>
+            <P><?= $user['lastName'] ?></P>
+            <P><?= $user['firstName'] ?></P>
+            <P><?= $user['birthDate'] ?></P>
+            <P><?= $user['card'] ?></P>
+            <P><?= $user['cardNumber'] ?></P>
+        </div>
+        <br><br>  
+        <?php            
+        }
     ?>
 </body>
 </html>
