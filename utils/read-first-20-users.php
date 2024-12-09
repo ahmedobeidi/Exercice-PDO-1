d2051cdef54042134fe0fd832ec0136499184cc9
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-09 14:16:07 +0100

    DONE: Exercice 3

---
+++ utils/read-first-20-users.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once 'connect-db.php';
+
+$sql = "SELECT * FROM `clients` ORDER BY id ASC LIMIT 10";
+
+try {
+    $stmt = $pdo->query($sql);
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $error) {
+    echo "Erreur lors de la requete : " . $error->getMessage();
+}
+?>
