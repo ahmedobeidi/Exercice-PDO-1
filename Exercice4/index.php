b731f733acac4314799d98791d9d56ccbe620dfb
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-11 12:26:11 +0100

    MODIFY: Ex 4

--- Exercice4/index.php
+++ Exercice4/index.php
@@ -10,8 +10,12 @@
     <title>Exercice 4</title>
 </head>
 <body>
-    <?php 
-        var_dump($users);
-    ?>
+    <h1>Les clients qui ont une carte de fidélité</h1>
+
+    <ul>
+        <?php foreach($clients as $client) { ?>
+            <li>Prénom : <?= $client['firstName']; ?> | Nom : <?= $client['lastName']; ?> | Date de naissance : <?= $client['birthDate']; ?> | Numéro de carte : <?= $client['cardNumber']; ?> | Type de carte : <?= $client['type']; ?></li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
--- utils/read-users-with-loyalty-card.php
+++ utils/read-users-with-loyalty-card.php
@@ -2,16 +2,19 @@
 
 require_once 'connect-db.php';
 
-$sql = "SELECT * FROM clients INNER JOIN cards ON cards.cardNumber = clients.cardNumber 
-        INNER JOIN cardtypes ON cardtypes.id = cards.cardTypesId 
-        WHERE clients.card = 1 AND cards.cardTypesId = 1 
-        AND cardtypes.id = 1";
+// $sql = "SELECT * FROM clients INNER JOIN cards ON cards.cardNumber = clients.cardNumber 
+//         INNER JOIN cardtypes ON cardtypes.id = cards.cardTypesId 
+//         WHERE clients.card = 1 AND cards.cardTypesId = 1 
+//         AND cardtypes.id = 1";
 
-$sql2;
+$sql = "SELECT clients.firstName, clients.lastName, clients.birthDate, clients.cardNumber, cardTypes.type
+        FROM clients
+        JOIN cards on cards.cardNumber = clients.cardNumber
+        JOIN cardtypes on cardtypes.id = cards.cardTypesId WHERE cardTypes.type = 'Fidélité'";
 
 try {
     $stmt = $pdo->query($sql);
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC); // ou fetch si vous savez que vous n'allez avoir qu'un seul résultat
+    $clients = $stmt->fetchAll(PDO::FETCH_ASSOC); // ou fetch si vous savez que vous n'allez avoir qu'un seul résultat
 
 } catch (PDOException $error) {
     echo "Erreur lors de la requete : " . $error->getMessage();
